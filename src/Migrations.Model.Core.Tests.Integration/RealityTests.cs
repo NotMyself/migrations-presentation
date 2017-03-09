981c79e04959b9549bfc55fd8f9d4633fd1d140b
Bobby Johnson <bobby.johnson@gmail.com>
2017-03-09 09:19:56 -0800

    adding test assemblies

---
+++ src/Migrations.Model.Core.Tests.Integration/RealityTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Migrations.Model.Tests.Integration
+{
+    public class RealityTests
+    {
+        [Fact]
+        public void TrueShouldBeTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
