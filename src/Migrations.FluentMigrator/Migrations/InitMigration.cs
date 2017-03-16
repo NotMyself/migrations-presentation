fa00124e895fa968380dc4f5bce9c9e856dd7698
Bobby Johnson <bobby.johnson@gmail.com>
2017-03-16 16:40:16 -0700

    Adding fluent migrations

---
+++ src/Migrations.FluentMigrator/Migrations/InitMigration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentMigrator;
+
+namespace Migrations.FluentMigrator.Migrations
+{
+    [Migration(1)]
+    public class InitMigration : Migration
+    {
+        public override void Down()
+        {
+            
+        }
+
+        public override void Up()
+        {
+            
+        }
+    }
+}
